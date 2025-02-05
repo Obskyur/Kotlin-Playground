eb832c1c5913402e941a7702cd61b304cae41b68
Obskyur <parkourman.miller@gmail.com>
2025-02-05 12:30:37 -0800

    learn: operators

---
+++ Playground/src/Operators.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val x: Int = 20
+    val y: Int = 30
+    val sum: Int = x + y
+    val diff: Int = x - y
+    val product: Int = x * y
+    val quotient: Float = x.toFloat() / y
+    val lt: Boolean = x < y
+    val gt: Boolean = x > y
+    val lte: Boolean = x <= y
+    val gte: Boolean = x >= y
+    val eq: Boolean = x == y
+    val neq: Boolean = x != y
+    println(quotient)
+}
