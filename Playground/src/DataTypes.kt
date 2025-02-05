42fcee135e95de26a8b91c620c4af7dd96df9e9c
Obskyur <parkourman.miller@gmail.com>
2025-02-05 12:30:27 -0800

    learn: data types

---
+++ Playground/src/DataTypes.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val personAge: Int = 20
+    val longValue: Long = 1649819516785984L
+    val area: Float = 223.5f
+    val pi: Double = 3.1415926535
+    val ch: Char = 'A'
+    val str: String = "Hello World"
+    val status: Boolean = true
+
+    println(ch)
+    print(str)
+}
